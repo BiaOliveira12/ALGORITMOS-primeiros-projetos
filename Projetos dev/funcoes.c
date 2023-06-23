424a6161983f84c7b0ae90c4167f1b1a0d3d4cf1
BiaOliveira12 <aguiarbeatriz12ba@gmail.com>
2023-06-23 17:23:12 -0300

    commit

--- Projetos dev/funcoes.c
+++ Projetos dev/funcoes.c
@@ -2,83 +2,88 @@
 #include <stdlib.h>
 #include <conio.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* Exercício sobre funções */
 
-float avista(float x){
-	float resp;
-	resp=x-x*0.1;
-	return resp;
+float precocomjuros;
+
+float avista(float x) {
+    float resp;
+    resp = x - x * 0.1;
+    return resp;
 }
-float duavez(float y){
-	float resp;
-	resp= y / 2;
-	return resp;
+
+float duasvezes(float x) {
+    float resp;
+    resp = x / 2;
+    return resp;
 }
-float trevez(float z){
-	float resp, jur;
-	jur = 0.015*z;
-	resp = jur + z / 3;
-	return resp;
+
+float tresvezes(float x) {
+    float resp;
+    resp = (x * 1.045) / 3;
+    precocomjuros = x * 1.045;
+    return resp;
 }
-float quavez(float c){
-	float resp, jur;
-	jur = 0.025*c;
-	resp = jur + c / 4;
+
+float quatrovezes(float x) {
+    float resp;
+    resp = (x * 1.1) / 4;
+    precocomjuros = x * 1.1;
+    return resp;
 }
 
 int main() {
-	float preco, precofinal;
-	char continua;
-	int opcao;
-	
-	
-	do{
-		
-		printf("\n\n\n\nDigite o valor do produto: \n");
-		scanf("%f", &preco);
-		
-		printf("[1] A Vista\n");
-		printf("[2] Em 2 vezes\n");
-		printf("[3] Em 3 vezes\n");
-		printf("[4] Em 4 vezes\n\n");
-		scanf(" %d", &opcao);
-	switch(opcao){
-	case 1:
-		printf("\nOpcao 1: \n\n");
-		precofinal=avista(preco);
-		printf("\n\tParcela 1 de 1:  R$ %.2f\n", precofinal);
-		printf("\tTotal a pagar: R$ %.2f\n", precofinal);
-		break;
-	case 2:
-		printf("\nOpcao 2: \n\n");
-		precofinal = duavez(preco);
-		printf("\n\tParcela 1 de 2:  R$ %.2f\n", precofinal);
-		printf("\tParcela 2 de 2:  R$ %.2f\n", precofinal);
-		printf("\tTotal a pagar: R$ %.2f\n", preco);
-		break;
-	case 3:
-		printf("\nOpcao 3: \n\n");
-		precofinal = trevez(preco);
-		printf("\n\tParcela 1 de 3:  R$ %.2f\n", precofinal);
-		printf("\tParcela 2 de 3:  R$ %.2f\n", precofinal+precofinal*0.015);
-		printf("\tParcela 3 de 3:  R$ %.2f\n", precofinal+precofinal*0.03);
-		printf("\tTotal a pagar: R$ %.2f\n", preco);
-		break;
-	case 4:
-		printf("\nOpcao 4: \n\n");
-		precofinal = quavez(preco);
-		printf("\n\tParcela 1 de 4:  R$ %.2f\n", precofinal);
-		printf("\tParcela 2 de 4:  R$ %.2f\n", precofinal+(precofinal*0.025));
-		printf("\tParcela 3 de 4:  R$ %.2f\n", precofinal+(precofinal*0.05));
-		printf("\tParcela 4 de 4:  R$ %.2f\n", precofinal+(precofinal*0.075));
-		printf("\tTotal a pagar: R$ %.2f\n", preco);
-		break;
-	default:
-		printf("Opcao invalida!");
-		break;
-	}
-	printf("\n\nDeseja continuar? (S/N)\n\n");
-	continua = getche();
-	}while((continua=='S')||(continua=='s'));
-	return 0;
+    float preco, precofinal;
+    char continua;
+    int opcao;
+
+    do {
+        system("cls");
+        printf("\n\nDigite o valor do produto: \n");
+        scanf("%f", &preco);
+        printf("\n\n\n[1] A Vista\n");
+        printf("[2] Em 2 vezes\n");
+        printf("[3] Em 3 vezes\n");
+        printf("[4] Em 4 vezes\n\n");
+        scanf(" %d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                precofinal = avista(preco);
+                printf("\nParcela 1 de 1:  R$ %.2f", precofinal);
+                printf("\nTotal a se Pagar: R$ %.2f", precofinal);
+                break;
+            case 2:
+                precofinal = duasvezes(preco);
+                printf("\nParcela 1 de 2: R$ %.2f", precofinal);
+                printf("\nParcela 2 de 2: R$ %.2f", precofinal);
+                printf("\nTotal a se Pagar: R$ %.2f", preco);
+                break;
+            case 3:
+                precofinal = tresvezes(preco);
+                printf("\nParcela 1 de 3: R$ %.2f", precofinal);
+                printf("\nParcela 2 de 3: R$ %.2f", precofinal);
+                printf("\nParcela 3 de 3: R$ %.2f", precofinal);
+                printf("\nTotal a se Pagar: R$ %.2f", precocomjuros);
+                break;
+            case 4:
+                precofinal = quatrovezes(preco);
+                printf("\nParcela 1 de 4: R$ %.2f", precofinal);
+                printf("\nParcela 2 de 4: R$ %.2f", precofinal);
+                printf("\nParcela 3 de 4: R$ %.2f", precofinal);
+                printf("\nParcela 4 de 4: R$ %.2f", precofinal);
+                printf("\nTotal a se Pagar: R$ %.2f", precocomjuros);
+                break;
+            default:
+                printf("Numero de Parcelas Indisponível");
+                break;
+        }
+
+        printf("\n\nContinua?\n\n");
+        printf("[S]im\n");
+        printf("[N]ao\n");
+        continua = getch();
+    } while ((continua == 'S') || (continua == 's'));
+
+    return 0;
 }
